ce6a346d0898c8ea8c02a227e005fdc856e9ddb1
ingdantev <ingdantev@gmail.com>
2024-02-10 21:52:51 +0000

    agregando los modulos de drive y docs

--- src/my-auth-google-drive/my-auth-google-drive.module.ts
+++ src/my-auth-google-drive/my-auth-google-drive.module.ts
@@ -1,7 +1,11 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { GoogleDocServiceService } from './services/GoogleDoc.Service';
+import { GoogleDriveService } from './services/GoogleDrive.Service';
+import { GoogleAutenticarService } from './services/GoogleDriveAutenticar.Service';
 @Module({
-    imports:[HttpModule]
+    imports:[HttpModule],
+    providers: [GoogleDocServiceService,GoogleDriveService,GoogleAutenticarService]
 })
 export class MyAuthGoogleDriveModule {
 
