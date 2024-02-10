7ac48da33a4d51130b406a04fd16c0f4237842bd
ingdantev <ingdantev@gmail.com>
2024-02-10 22:47:41 +0000

    provando a mostrar el id de aimagen

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(
     private readonly appService: AppService,
   ) {}
-  @Get('googleDrive')
+  @Get('saludo')
   saludo(){
     return "hola"
   }
@@ -28,6 +28,7 @@ export class AppController {
   async muestraimagen(
     @Body() idForGoogleElement:string
   ){
+    console.log(idForGoogleElement)
     return await this.appService.obtenerwebViewLink(idForGoogleElement)
   }
 
