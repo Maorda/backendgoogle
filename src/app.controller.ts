e712c48b6d1363e6540c486f065469b6fd3c8c48
ingdantev <ingdantev@gmail.com>
2024-02-10 22:35:02 +0000

    incorporando al controllador app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,9 +7,46 @@ export class AppController {
   constructor(
     private readonly appService: AppService,
   ) {}
-  @Get('saludo')
+  @Get('googleDrive')
   saludo(){
     return "hola"
+  }
+  @Post("subeimagen")
+  @UseInterceptors(
+    FileInterceptor('file')
+  )
+  async getHello(
+    @UploadedFile() file:Express.Multer.File,
+    @Body() idForGoogleElement: string
+    
+  ) {
+    return await this.appService.subirImagen(file,idForGoogleElement)
+    //return this.appService.getHello();
+  }
+
+  @Post('muestraimagen')
+  async muestraimagen(
+    @Body() idForGoogleElement:string
+  ){
+    return await this.appService.obtenerwebViewLink(idForGoogleElement)
+  }
+
+  @Post('crearcarpeta')
+  async crearCarpeta(
+    @Body() idForGoogleElement:string,
+    @Body() nameForGoogleElement:string
+  ){
+    return await this.appService.crearCarpeta(idForGoogleElement,nameForGoogleElement)
+  }
+  @Post('buscareemplaza')
+  async buscaReemplaza(){
+    return await this.appService.buscaReemplaza()
+
+  }
+  @Post('docx')
+  async docx(){
+    return await this.appService.plantillaDocx()
+
   }
 
   
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import { GoogleDriveService } from './managerGoogle/services/GoogleDrive.Service';
+import { GoogleDocService } from './managerGoogle/services/GoogleDoc.Service';
 
 
 @Injectable()
 export class AppService {
+  constructor(
+    private readonly googleDriveService: GoogleDriveService,
+    private readonly googleDocService:GoogleDocService
+    ){}
   getHello(): string {
     return 'Hello World!';
+  }
+  public async subirImagen(file: Express.Multer.File,idForGoogleElement:string): Promise<string> {
+    try {
+      const link = await this.googleDriveService.subirImagen(file,idForGoogleElement);
+      // do something with the link, e.g., save it to the database
+      return link;
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  public async obtenerwebViewLink(fileId: string): Promise<string> {
+    try {
+      const link = await this.googleDriveService.obtenerwebViewLink(fileId);
+      // do something with the link, e.g., return it to the user
+      return link;
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+  public async crearCarpeta(idForGoogleElement:string,nameForGoogleElement:string){
+    try{
+      await this.googleDriveService.crearCarpeta(idForGoogleElement,nameForGoogleElement)//carpeta logos
+
+    }catch (e){
+      throw new Error(e);
+
+    }
+  }
+  public async buscaReemplaza(){
+    try{
+      const idCopia = await this.googleDocService.creaCopia('1hXE80EcY-ZiY3WHN0OkJZzR_iD3d5-n1-_4jGEIM8Zs',"my copia",'1B3aTsga8DljMwFO-d5djpi4E-S5h_8os')//1-Lenn5pGOvFa8lDeLDShsci8CM1g_JxquZhQJwj6c1s
+      const cambiado = await this.googleDocService.buscaReemplaza(["<nombre>","<apellido>"],["dante","manrique"],idCopia)
+      //console.log(cambiado.data.replies)
+      this.googleDocService.insertaImagenCuerpo("https://cdn.sstatic.net/Sites/stackoverflow/company/img/logos/so/so-logo.png",idCopia)
+      this.googleDocService.insertaParrafo(["hola amigo que es de tu vida","por donde has estado que no se te ha visto","en tanto tiempo"],idCopia)
+
+    }catch (e){
+      throw new Error(e);
+
+    }
+  }
+  public async plantillaDocx(){
+    return await this.googleDocService.insertaParrafoDocx(["s"],"")
   }
   
 }
