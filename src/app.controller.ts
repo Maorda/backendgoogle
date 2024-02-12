4cbec20f5ffeb8189389716859a1583d8e5ec9e7
Maorda <ingdantev@gmail.com>
2024-02-12 18:05:17 -0500

    eliminando vecel y configurando package.json cyclic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,6 @@
 import { Controller, Get, Post, UseGuards,Request, UploadedFile, Body, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
-import { GoogleOAuthGuard } from './google-oauth.guard';
-import { GoogleDriveService } from './managerdrive/services/googleDriveService';
-
 
 
 @Controller("googleDrive")
